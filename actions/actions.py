bb370c99ab569c92b38254ac380d8db04efe2164
Raul Del Pozo Moreno <rauldpm@correo.ugr.es>
2022-06-23 12:02:33 +0200

    Enhance user recognition

--- actions/actions.py
+++ actions/actions.py
@@ -37,6 +37,7 @@ from rasa_sdk.types import DomainDict
 names = pathlib.Path("data/diccionarios/nombres.txt").read_text().split("\n")
 malsonantes = pathlib.Path("data/diccionarios/malsonante.txt").read_text().split("\n")
 
+
 class ValidateNameForm(FormValidationAction):
 
     def name(self) -> Text:
@@ -50,11 +51,9 @@ class ValidateNameForm(FormValidationAction):
         print("Required slots")
 
         first_name = tracker.slots.get("first_name")
-
         if first_name is not None:
             if first_name.lower() not in names:
                 return ["name_spelled_correctly"] + slots_mapped_in_domain
-
         return slots_mapped_in_domain
 
     async def extract_name_spelled_correctly(
@@ -62,10 +61,12 @@ class ValidateNameForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         print("extract_name_spelled_correctly")
 
-        if len(tracker.slots.get("first_name")) <= 2:
-            dispatcher.utter_message(text=f"Ese nombre es muy corto, dime otro mas largo (minimo 3 letras)")
-            return {"first_name": None, "name_spelled_correctly": False, "first_name_save": None}
-
+        first_name = tracker.slots.get("first_name")
+        if first_name != "/repetir_nombre":
+            if len(first_name) < 3 or len(first_name) > 11:
+                dispatcher.utter_message(
+                    text=f"El nombre no cumple con los requisitos (minimo 3 letras y maximo 11)")
+                return {"first_name": None, "name_spelled_correctly": False, "first_name_save": None}
         return {"name_spelled_correctly": tracker.get_intent_of_latest_message() == "affirm"}
 
     def validate_name_spelled_correctly(
@@ -73,8 +74,10 @@ class ValidateNameForm(FormValidationAction):
         dispatcher: CollectingDispatcher,
         tracker: Tracker, domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `first_name` value."""
         print("validate_name_spelled_correctly")
+
+        if tracker.get_intent_of_latest_message() == "stop":
+            return {"requested_slot": None, "first_name": None, "name_spelled_correctly": None, "first_name_set": None}
 
         intent = tracker.get_intent_of_latest_message()
         first_name = tracker.slots.get("first_name")
@@ -89,24 +92,25 @@ class ValidateNameForm(FormValidationAction):
 
         if (first_name_save is not None and intent == "affirm" and first_name_save != "/repetir_nombre"):
             names.append(first_name_save.lower())
-            return {"first_name": first_name_save, "name_spelled_correctly": None, "first_name_save": None}
+            return {"first_name": first_name_save, "first_name_set": True, "first_name_save": None}
 
         elif (first_name_save is not None) and ((intent == "repetir_nombre") or (first_name == "/repetir_nombre")):
             if (first_name_save is not None and latest_message is not None and latest_message != first_name):
                 return {"first_name": latest_message, "name_spelled_correctly": None, "first_name_save": None}
             if (first_name_save.lower() not in names and intent == "repetir_nombre" and first_name != "/repetir_nombre") or (intent == "deny"):
-                buttons = [{'title': 'Yes', 'payload': '/affirm'}, {'title': 'No', 'payload': '/repetir_nombre'}]
-                dispatcher.utter_message('Mmm esta bien escrito tu nombre? Si no, escribe tu nombre de nuevo', buttons=buttons)
+                buttons = [{'title': 'Yes', 'payload': '/affirm'},
+                           {'title': 'No', 'payload': '/repetir_nombre'}]
+                dispatcher.utter_message("Mmm esta bien escrito tu nombre? Si no, escribe tu nombre de nuevo", buttons=buttons)
                 return {"first_name": None, "name_spelled_correctly": None, "first_name_save": latest_message}
             return {"first_name": None, "name_spelled_correctly": False, "first_name_save": latest_message}
 
         if tracker.get_intent_of_latest_message() != "repetir_nombre" or intent == "repetir_nombre":
             if latest_message is not None and latest_message != tracker.get_slot("first_name") and intent != "affirm":
                 return {"first_name": latest_message, "name_spelled_correctly": None}
-            buttons = [{'title': 'Yes', 'payload': '/affirm'}, {'title': 'No', 'payload': '/repetir_nombre'}]
-            dispatcher.utter_message('Mmm esta bien escrito tu nombre? Si no, escribe tu nombre de nuevo', buttons=buttons)
+            buttons = [{'title': 'Yes', 'payload': '/affirm'},
+                       {'title': 'No', 'payload': '/repetir_nombre'}]
+            dispatcher.utter_message("Mmm esta bien escrito tu nombre? Si no, escribe tu nombre de nuevo", buttons=buttons)
             return {"first_name": None, "name_spelled_correctly": None, "first_name_save": tracker.get_slot("first_name")}
-
         return {"first_name": tracker.get_slot("first_name"), "name_spelled_correctly": None}
 
     def validate_first_name(
@@ -116,12 +120,22 @@ class ValidateNameForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `first_name` value."""
+        print("Validate first_name value")
+
+        if tracker.get_intent_of_latest_message() == "stop":
+            return {"requested_slot": None, "first_name": None, "name_spelled_correctly": None, "first_name_set": None}
+
         if slot_value is not None:
             print(f"First name given = {slot_value} length = {len(slot_value)}")
-            if len(slot_value) <= 2:
-                return {"first_name": None, "name_spelled_correctly": False }
+            if (len(slot_value) < 3 or len(slot_value) > 11) and (slot_value != "/repetir_nombre"):
+                dispatcher.utter_message(text=f"El nombre no cumple con los requisitos (minimo 3 letras y maximo 11)")
+                return {"first_name": None, "name_spelled_correctly": False}
             else:
+<<<<<<< HEAD
                 return {"first_name": slot_value}
         return {"first_name": None, "name_spelled_correctly": False }
 >>>>>>> e7fd007 (Added unrecognized username loop, added more username recognition)
+=======
+                return {"first_name": slot_value, "first_name_set": True}
+        return {"first_name": None, "name_spelled_correctly": False}
+>>>>>>> 8edb96e (Enhance user recognition)
