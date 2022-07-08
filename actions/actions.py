48e60e825d3a935a59e5170b47f83dea4697faec
Raul Del Pozo Moreno <rauldpm@correo.ugr.es>
2022-07-08 20:22:38 +0200

    Added booking user iteraction

--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,4 @@
-from asyncore import dispatcher
-from code import interact
-from copyreg import dispatch_table
 import pathlib
-from sre_constants import CATEGORY_SPACE
-from typing import Text, List, Any, Dict, Optional
 import json
 
 from rasa_sdk import Tracker, FormValidationAction
@@ -11,6 +6,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from rasa_sdk.interfaces import Action
 from rasa_sdk.events import SlotSet
+from typing import Text, List, Any, Dict, Optional
 
 names = pathlib.Path("data/diccionarios/nombres.txt").read_text().split("\n")
 malsonantes = pathlib.Path(
@@ -24,6 +20,9 @@ f = open(pathlib.Path("data/menu/horario.json"))
 horario = json.load(f)
 f.close()
 
+f = open(pathlib.Path("data/tables/mesas.json"))
+mesas = json.load(f)
+f.close()
 
 class ValidateNameForm(FormValidationAction):
 
@@ -191,13 +190,13 @@ class GetMenu(Action):
         if intent == "menu" or intent == "establecer_menu":
             for item in CATEGORIA_MENU:
                 dispatcher.utter_message(self.getSubmenu(item))
-            return [SlotSet("plato_id", None), SlotSet("categoria", categoria)]
+            return [SlotSet("cantidad", None), SlotSet("categoria", categoria)]
         elif categoria in CATEGORIA_MENU:
             dispatcher.utter_message(self.getSubmenu(categoria))
-            return [SlotSet("plato_id", None), SlotSet("categoria", categoria)]
+            return [SlotSet("cantidad", None), SlotSet("categoria", categoria)]
 
         dispatcher.utter_message(text=f'No se ha validado la la categoria.')
-        return [SlotSet("plato_id", None), SlotSet("categoria", None)]
+        return [SlotSet("cantidad", None), SlotSet("categoria", None)]
 
 
 class ResetMenu(Action):
@@ -216,7 +215,9 @@ class ResetMenu(Action):
                         "pescado_nombre", None), SlotSet("pescado_precio", None),
                     SlotSet("postre_id", None), SlotSet(
                         "postre_nombre", None), SlotSet("postre_precio", None),
-                    SlotSet("bebida_id", None), SlotSet("entrante_nombre", None), SlotSet("entrante_precio", None)]
+                    SlotSet("bebida_id", None), SlotSet(
+                        "entrante_nombre", None), SlotSet("entrante_precio", None),
+                    SlotSet("plato_establecido", False), SlotSet("cantidad", None)]
         elif intent == "deny":
             dispatcher.utter_message(
                 text=f'El menu que tienes establecido es el siguiente:')
@@ -224,8 +225,17 @@ class ResetMenu(Action):
             return ''
         elif intent in ["borrar_entrante", "borrar_carne", "borrar_pescado", "borrar_postre", "borrar_bebida"]:
             tipo = intent.split("_", 1)[1]
-            dispatcher.utter_message(text=f'Se ha eliminado el plato seleccionado.')
-            return [SlotSet(tipo+"_id", None), SlotSet(tipo+"_nombre", None), SlotSet(tipo+"_precio", None)]
+            dispatcher.utter_message(
+                text=f'Se ha eliminado el plato seleccionado.')
+            entrante_id = tracker.get_slot("entrante_id")
+            carne_id = tracker.get_slot("carne_id")
+            pescado_id = tracker.get_slot("pescado_id")
+            postre_id = tracker.get_slot("postre_id")
+            bebida_id = tracker.get_slot("bebida_id")
+            if entrante_id is None and carne_id is None and pescado_id is None and postre_id is None and bebida_id is None:
+                return [SlotSet(tipo+"_id", None), SlotSet(tipo+"_nombre", None), SlotSet(tipo+"_precio", None), SlotSet("plato_establecido", False), SlotSet("cantidad", None)]
+            else:
+                return [SlotSet(tipo+"_id", None), SlotSet(tipo+"_nombre", None), SlotSet(tipo+"_precio", None), SlotSet("cantidad", None)]
         else:
             return ''
 
@@ -319,9 +329,9 @@ class GetUserMenu(Action):
         return ''
 
 
-class ValidatePlatoForm(FormValidationAction):
+class ValidateCantidadForm(FormValidationAction):
     def name(self) -> Text:
-        return 'validate_plato_form'
+        return 'validate_cantidad_form'
 
     async def required_slots(
         self, slots_mapped_in_domain: List[Text],
@@ -330,22 +340,23 @@ class ValidatePlatoForm(FormValidationAction):
     ) -> Optional[List[Text]]:
         return slots_mapped_in_domain
 
-    def validate_plato_id(
+    def validate_cantidad(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        print("validate_plato_id")
+        print("validate_cantidad")
+
+        intent = tracker.get_intent_of_latest_message()
+        if intent == "stop":
+            return {"requested_slot": None, "cantidad": None, "categoria": None}
 
         categoria = tracker.get_slot("categoria")
-        categoria = categoria.lower()
-        plato_id = tracker.get_slot("plato_id")
-        intent = tracker.get_intent_of_latest_message()
-
-        if intent == "stop":
-            return {"requested_slot": None, "first_name": None}
+        if categoria is not None:
+            categoria = categoria.lower()
+        cantidad = tracker.get_slot("cantidad")
 
         if categoria is not None:
             for it in CATEGORIA_MENU:
@@ -354,45 +365,239 @@ class ValidatePlatoForm(FormValidationAction):
                     break
 
             if intent in ["establecer_plato_entrante", "establecer_plato_carne", "establecer_plato_pescado", "establecer_plato_postre", "establecer_plato_bebida"]:
-                return {"categoria": categoria, "plato_id": None}
+                return {"categoria": categoria, "cantidad": None}
 
             f = open(pathlib.Path("data/menu/"+str(categoria)+".json"))
             data = json.load(f)
             f.close()
 
             for it in data[categoria]:
-                if str(it['id']) == plato_id:
+                if str(it['id']) == cantidad:
                     if categoria == "entrantes":
                         dispatcher.utter_message(
                             response="utter_entrante_seleccionado", entrante_nombre=it['nombre'], entrante_precio=it['precio'])
                         dispatcher.utter_message(
                             response="utter_change_plato_info")
-                        return {"categoria": None, "plato_id": plato_id, "entrante_id": plato_id, "entrante_nombre": it['nombre'], "entrante_precio": it['precio']}
+                        return {"categoria": None, "cantidad": cantidad, "entrante_id": cantidad, "entrante_nombre": it['nombre'], "entrante_precio": it['precio'], "plato_establecido": True}
                     elif categoria == "carnes":
                         dispatcher.utter_message(
                             response="utter_carne_seleccionado", carne_nombre=it['nombre'], carne_precio=it['precio'])
                         dispatcher.utter_message(
                             response="utter_change_plato_info")
-                        return {"categoria": None, "plato_id": plato_id, "carne_id": plato_id, "carne_nombre": it['nombre'], "carne_precio": it['precio']}
+                        return {"categoria": None, "cantidad": cantidad, "carne_id": cantidad, "carne_nombre": it['nombre'], "carne_precio": it['precio'], "plato_establecido": True}
                     elif categoria == "pescados":
                         dispatcher.utter_message(
                             response="utter_pescado_seleccionado", pescado_nombre=it['nombre'], pescado_precio=it['precio'])
                         dispatcher.utter_message(
                             response="utter_change_plato_info")
-                        return {"categoria": None, "plato_id": plato_id, "pescado_id": plato_id, "pescado_nombre": it['nombre'], "pescado_precio": it['precio']}
+                        return {"categoria": None, "cantidad": cantidad, "pescado_id": cantidad, "pescado_nombre": it['nombre'], "pescado_precio": it['precio'], "plato_establecido": True}
                     elif categoria == "postres":
                         dispatcher.utter_message(
                             response="utter_postre_seleccionado", postre_nombre=it['nombre'], postre_precio=it['precio'])
                         dispatcher.utter_message(
                             response="utter_change_plato_info")
-                        return {"categoria": None, "plato_id": plato_id, "postre_id": plato_id, "postre_nombre": it['nombre'], "postre_precio": it['precio']}
+                        return {"categoria": None, "cantidad": cantidad, "postre_id": cantidad, "postre_nombre": it['nombre'], "postre_precio": it['precio'], "plato_establecido": True}
                     elif categoria == "bebidas":
                         dispatcher.utter_message(
                             response="utter_bebida_seleccionado", bebida_nombre=it['nombre'], bebida_precio=it['precio'])
                         dispatcher.utter_message(
                             response="utter_change_plato_info")
-                        return {"categoria": None, "plato_id": plato_id, "bebida_id": plato_id, "bebida_nombre": it['nombre'], "bebida_precio": it['precio']}
+                        return {"categoria": None, "cantidad": cantidad, "bebida_id": cantidad, "bebida_nombre": it['nombre'], "bebida_precio": it['precio'], "plato_establecido": True}
 
-        dispatcher.utter_message(
-            text=f'El plato elegido no esta en la lista de la categoria, selecciona otro o escribe "stop" para parar el proceso.')
-        return {"plato_id": None}
+            dispatcher.utter_message(
+                text=f'El plato elegido no esta en la lista de la categoria, selecciona otro o escribe "stop" para parar el proceso.')
+            return {"cantidad": None}
+
+
+class ValidateComensales(Action):
+    def name(self):
+        return 'ValidateComensales'
+
+    def run(self, dispatcher, tracker, domain):
+        print("ValidateComensales")
+
+        intent = tracker.get_intent_of_latest_message()
+        if intent == "stop":
+            dispatcher.utter_message(response="utter_stop")
+            return {"requested_slot": None, "comensales": None}
+
+        comensales = tracker.get_slot("comensales")
+
+        if comensales is not None:
+
+            if not comensales.isnumeric():
+                dispatcher.utter_message(
+                    text='No se ha podido validar el numero de comensales. Introducelo de nuevo <Reserva para X persona>.')
+                return [SlotSet("comensales", None)]
+
+            if int(comensales) <= 0:
+                dispatcher.utter_message(
+                    text=f'El numero de comensales debe ser superior a 0, si quieres parar escribe "stop".')
+                return [SlotSet("comensales", None)]
+
+            disponibles = len(
+                GetMesasLibres.get_mesas_libres_numero_comensal(self, comensales))
+            if disponibles <= 0:
+                dispatcher.utter_message(
+                    text=f'No disponemos de mesas libres en este momento para ' + comensales + ' comensales.')
+                return [SlotSet("comensales", None), SlotSet("mesa_disponible", False)]
+            message = 'Para ' + comensales + ' comensales disponemos de ' + \
+                str(disponibles) + ' mesas libres.'
+            dispatcher.utter_message(text=message)
+            return [SlotSet("comensales", comensales), SlotSet("mesa_disponible", True)]
+
+
+class ReservarMesa(Action):
+    def name(self):
+        return 'ReservarMesa'
+
+    def updateMesasJsonFile(Action, mesa):
+        jsonFile = open("data/tables/mesas.json", "w")
+        jsonFile.write(json.dumps(mesas))
+        jsonFile.close()
+        return ''
+
+    def run(self, dispatcher, tracker, domain):
+        print("ReservarMesa")
+        intent = tracker.get_intent_of_latest_message()
+
+        mesa_reservada = tracker.get_slot("mesa_reservada")
+        if mesa_reservada is not None:
+            dispatcher.utter_message(
+                text='Ya tienes realizada una reserva, si quieres cambiarla, eliminala primero introduciendo <Borrar reserva>.')
+            return ''
+
+        if intent == "affirm":
+            comensales = tracker.get_slot("comensales")
+            mesas_disponibles = GetMesasLibres.get_mesas_libres_numero_comensal(
+                self, comensales)
+            if len(mesas_disponibles) > 0:
+                id = mesas['mesas'][mesas_disponibles[0]]['id']
+                mesas['mesas'][mesas_disponibles[0]]['comensales'] = comensales
+                mesas['mesas'][mesas_disponibles[0]]['ocupada'] = "True"
+                self.updateMesasJsonFile(mesas)
+                dispatcher.utter_message(text=f'Mesa reservada.')
+                return [SlotSet("mesa_reservada", id)]
+            else:
+                dispatcher.utter_message(
+                    text=f'No he encontrado mesas libres.')
+                return [SlotSet("comensales", None), SlotSet("mesa_reservada", None), SlotSet("mesa_disponible", False)]
+        elif intent == "deny":
+            dispatcher.utter_message(text=f'Cancelando reserva de mesa.')
+        return [SlotSet("comensales", None), SlotSet("mesa_reservada", None), SlotSet("mesa_disponible", None)]
+
+
+class RemoveMesa(Action):
+    def name(self):
+        return 'RemoveMesa'
+
+    def updateMesasJsonFile(Action, mesa):
+        jsonFile = open("data/tables/mesas.json", "w")
+        jsonFile.write(json.dumps(mesas))
+        jsonFile.close()
+        return ''
+
+    def run(self, dispatcher, tracker, domain):
+        print('RemoveMesa')
+
+        intent = tracker.get_intent_of_latest_message()
+        comensales = tracker.get_slot("comensales")
+        mesa_reservada = tracker.get_slot("mesa_reservada")
+
+        if mesa_reservada is None:
+            dispatcher.utter_message(
+                text=f'Antes de eliminar debes realizar una reserva.')
+            return ''
+
+        if intent in ["affirm", "deny"]:
+            if comensales is None:
+                dispatcher.utter_message(
+                    text=f'No se ha establecido el numero de comensales, para hacerlo, establece una reserva antes.')
+                return ''
+            elif mesa_reservada is None:
+                dispatcher.utter_message(
+                    text=f'No se ha establecido una mesa reservada, para hacerlo, establece una reserva antes.')
+                return ''
+
+        if intent == "affirm":
+            mesas['mesas'][mesa_reservada]['comensales'] = "0"
+            mesas['mesas'][mesa_reservada]['ocupada'] = "False"
+            self.updateMesasJsonFile(mesas)
+            dispatcher.utter_message(
+                text=f'Se ha cancelado la reserva con exito.')
+            return [SlotSet("comensales", None), SlotSet("mesa_reservada", None)]
+        elif intent == "deny":
+            dispatcher.utter_message(text=f'Operacion cancelada. La reserva se mantiene para ' +
+                                     str(comensales) + ' comensales en la mesa ' + str(mesa_reservada) + '.')
+            return ''
+        return ''
+
+
+class MostrarReserva(Action):
+    def name(self):
+        return 'MostrarReserva'
+
+    def run(self, dispatcher, tracker, domain):
+        print('MostrarReserva')
+
+        mesa_reservada = tracker.get_slot("mesa_reservada")
+        if mesa_reservada is not None:
+            comensales = tracker.get_slot("comensales")
+            dispatcher.utter_message(text=f'Tienes una reserva para ' +
+                                     str(comensales) + ' comensales en la mesa ' + str(mesa_reservada) + '.')
+            return ''
+        else:
+            dispatcher.utter_message(
+                text=f'No se ha establecido una mesa reservada, para hacerlo, establece una reserva antes.')
+            return ''
+
+
+class GetMesasLibres(Action):
+    def name(self):
+        return 'GetMesasLibres'
+
+    def get_mesas_libres(Action):
+        disponibles = []
+        for it in mesas['mesas']:
+            if it['ocupada'] == "False":
+                disponibles.append(it['id'])
+        return disponibles
+
+    def get_mesas_libres_numero_comensal(Action, numero):
+        disponibles = []
+        for it in mesas['mesas']:
+            if it['ocupada'] == "False" and int(it['max_comensales']) >= int(numero):
+                disponibles.append(it['id'])
+        return disponibles
+
+    def run(self, dispatcher, tracker, domain):
+        print("GetMesasLibres")
+
+        intent = tracker.get_intent_of_latest_message()
+        disponibles = self.get_mesas_libres()
+        if intent == "mostrar_cantidad_mesas_libres" or intent == "hacer_reserva":
+
+            if len(disponibles) > 0:
+                message = "Tenemos disponibles: " + "\n"
+                m = []
+                n = {}
+                for id in disponibles:
+                    m.append(mesas['mesas'][id]['max_comensales'])
+                ini = m[0]
+                contador = 0
+                for it in m:
+                    if ini != it:
+                        ini = it
+                        contador = 0
+                    contador += 1
+                    n[ini] = contador
+                for it in n:
+                    message += "- " + \
+                        str(n[it]) + " mesas de " + it + " comensales." + "\n"
+
+                message = message[:-2] + " comensales."
+                dispatcher.utter_message(text=message)
+
+            return [SlotSet("cantidad", None), SlotSet("mesa_disponible", True)]
+
+        return [SlotSet("cantidad", None), SlotSet("mesa_disponible", False)]
