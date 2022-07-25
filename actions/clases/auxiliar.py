a676f38e8737c98ce3995512618f1f3cfab1b627
Raul Del Pozo Moreno <rauldpm@correo.ugr.es>
2022-07-25 17:20:07 +0200

    Add dinamic options class

--- actions/clases/auxiliar.py
+++ actions/clases/auxiliar.py
@@ -1,4 +1,4 @@
-
+from rasa_sdk.interfaces import Action
 import pathlib
 from os import path
 import json
@@ -56,3 +56,39 @@ class Auxiliar():
 
     def get_data_path(self):
         return self.data_path
+
+class Opciones(Action):
+    def name(self):
+        return "Opciones"
+
+    def run(self, dispatcher, tracker, domain):
+        print("Opciones")
+
+        buttons = []
+        buttons = [
+            {'title': 'Opciones', 'payload': '/opciones'},
+            {'title': 'Horario', 'payload': '/horario'},
+            {'title': 'Consultar carta', 'payload': '/menu_completo'}
+        ]
+
+        if tracker.get_slot("first_name_set"):
+            buttons.append({"title": "Olvidame", "payload": "/identificar_borrar"})
+        else:
+            buttons.append({"title": "Identificarme", "payload": "/identificar"})
+
+        if tracker.get_slot("menu_establecido"):
+            buttons.append({"title": "Borrar menu", "payload": "/menu_borra_todo"})
+            buttons.append({"title": "Borrar plato", "payload": "/menu_borra_plato"})
+            buttons.append({"title": "Consultar mi menu", "payload": "/menu_usuario"})
+        else:
+            buttons.append({"title": "Establecer menu", "payload": "/establecer_menu"})
+
+        if tracker.get_slot("reserva_dia") or tracker.get_slot("reserva_hora") or tracker.get_slot("reserva_comensales") or tracker.get_slot("reserva_completa"):
+            buttons.append({"title": "Borrar reserva", "payload": "/reserva_borrar_mesa"})
+            buttons.append({"title": "Finalizar reserva", "payload": "/finalizar_reserva"})
+            buttons.append({"title": "Consultar mi reserva", "payload": "/reserva_usuario"})
+        else:
+            buttons.append({"title": "Realizar reserva de mesa", "payload": "/reservar"})
+
+        dispatcher.utter_message(text=f'Estas son las opciones que puedes realizar ahora', buttons=buttons, button_type="vertical")
+        return ""
