ccf0947c581e4f71fcad1f27a045bbede1f022b1
Raul Del Pozo Moreno <rauldpm@correo.ugr.es>
2022-07-29 12:34:08 +0200

    Remove outdated file

--- Docker/server/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set secrets
-source /app/secrets/telegram_secrets.env
-
-# Run rasa
-rasa run -m /app/models --enable-api --cors * --debug --endpoints endpoints.yml --log-file logs/rasa.log --debug
